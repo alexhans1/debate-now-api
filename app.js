695e94da7a153ad893828b7f49b08986d1eab98d
alexhans1 <alexander.hans.mail@gmail.com>
2017-11-01 23:09:24 +0100

    disable cors on production as well

--- app.js
+++ app.js
@@ -14,38 +14,36 @@ app.use(cookieParser());
 // setup database connection
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-	(app.get('env') === 'production') ? process.env.DB_NAME : 'debate-now',
-	(app.get('env') === 'production') ? process.env.DB_USER_NAME : 'root',
-	(app.get('env') === 'production') ? process.env.DB_PASSWORD : process.env.DB_PASSWORD_LOCAL,
-	{
-		host: (app.get('env') === 'production') ? process.env.DB_HOST : 'localhost',
-		dialect: 'mysql',
+  (app.get('env') === 'production') ? process.env.DB_NAME : 'debate-now',
+  (app.get('env') === 'production') ? process.env.DB_USER_NAME : 'root',
+  (app.get('env') === 'production') ? process.env.DB_PASSWORD : process.env.DB_PASSWORD_LOCAL,
+  {
+    host: (app.get('env') === 'production') ? process.env.DB_HOST : 'localhost',
+    dialect: 'mysql',
 
-		pool: {
-			max: 5,
-			min: 0,
-			idle: 10000
-		},
-	}
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+  }
 );
 
 // test the connection
 sequelize.authenticate()
 .then(() => {
-	console.log('Connection has been established successfully.');
+  console.log('Connection has been established successfully.');
 })
 .catch(err => {
-	console.error('Unable to connect to the database:', err);
+  console.error('Unable to connect to the database:', err);
 });
 
 // disable CORS when local
-if (process.env.NODE_ENV !== 'production') {
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-}
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 //setup routes URIs
 const userApi = require('./routes/userApi')(sequelize);
@@ -57,20 +55,20 @@ app.use('/event', eventApi);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	let err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(port);

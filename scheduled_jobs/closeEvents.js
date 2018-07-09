8ca79a1fc6ebd7744d3104ae80b9873df0312a81
alexhans1 <mail@alexanderhans.me>
2018-07-09 16:45:34 +0200

    test time

--- scheduled_jobs/closeEvents.js
+++ scheduled_jobs/closeEvents.js
@@ -2,7 +2,7 @@ module.exports = (sequelize) => {
   const schedule = require('node-schedule');
   const Event = require('../models/events')(sequelize);
 
-  schedule.scheduleJob('44 16 * * 1', () => {
+  schedule.scheduleJob('47 19 * * 1', () => {
     console.log('Run Scheduled Job: Close Open and Old Events.');
     try {
       Event.update({
